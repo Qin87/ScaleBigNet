ac44b8c6c683dd4ec331cb5746fb6534c6471ac7
Qin87 <qinjiang19871018@gmail.com>
2025-07-27 23:13:16 +0100

    --k_plus="$k"

--- scaleNet.sh
+++ scaleNet.sh
@@ -4,9 +4,11 @@ Direct_dataset=( 'chameleon' 'arxiv-year'  'snap-patents' 'directed-roman-empire
 
 # Run sequentially
 for Didataset in "${Direct_dataset[@]}"; do
+  for k in 1 2 3 4 5; do
     echo "Running with $Didataset..."
-    python -m  src.run   --dataset="$Didataset" --use_best_hyperparams
+    python -m  src.run   --dataset="$Didataset"  --k_plus="$k"  --use_best_hyperparams
     echo "Finished $layer."
+done
 done
 
 echo "All runs completed."
