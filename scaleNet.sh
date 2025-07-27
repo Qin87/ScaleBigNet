a2358aea04b23e6d1d7e19a15e20af0f272f33d9
Qin87 <qinjiang19871018@gmail.com>
2025-07-27 18:52:50 +0100

    ready to run

--- scaleNet.sh
+++ scaleNet.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-Direct_dataset=( 'arxiv-year'  'snap-patents' 'directed-roman-empire' )  #
+Direct_dataset=( 'chameleon' 'arxiv-year'  'snap-patents' 'directed-roman-empire' )  #
 
 # Run sequentially
 for Didataset in "${Direct_dataset[@]}"; do
     echo "Running with $Didataset..."
-    python -m  src.run   --dataset="$Didataset"     > "July17_${Didataset}.log" 2>&1
-    echo "Finished seed $layer."
+    python -m  src.run   --dataset="$Didataset"
+    echo "Finished $layer."
 done
 
 echo "All runs completed."
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,7 +1,7 @@
 import os
 import yaml
-
 import torch
+from datetime import datetime
 
 
 def use_best_hyperparams(args, dataset_name):
@@ -31,6 +31,14 @@ def get_available_accelerator():
 
 
 
+def log_file(net_to_print, dataset_to_print, args):
+    log_file_name = dataset_to_print+'_'+args.conv_type+'_'+net_to_print+'_lay'+str(args.num_layers)+'_lr'+str(args.lr)+'_split'+str(args.num_runs)
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    log_file_name_with_timestamp = f"{log_file_name}_{timestamp}.log"
 
+    log_directory = "~/Documents/Benlogs/"  # Change this to your desired directory
+    log_directory = os.path.expanduser(log_directory)
+
+    return log_directory, log_file_name_with_timestamp
 
 
