6ef614e5adb206c9e0c028c7050f04b0447e9c08
Qin87 <qinjiang19871018@gmail.com>
2025-08-01 10:15:58 +0100

    run faber

--- scaleNet.sh
+++ scaleNet.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-Direct_dataset=( 'chameleon' 'arxiv-year'  'snap-patents' 'directed-roman-empire' )  #
+Direct_dataset=( 'squirrel' )  # 'arxiv-year'  'snap-patents' 'directed-roman-empire'
 
-K_values=(1 2 3 4 5)
-Weight_penalties=("None" "exp" "lin" )
+K_values=(1 2 3 )
+Weight_penalties=(  "lin" )
 
 # Run sequentially
 for Didataset in "${Direct_dataset[@]}"; do
     for k in "${K_values[@]}"; do
         for penalty in "${Weight_penalties[@]}"; do
             echo "Running with dataset=$Didataset, k_plus=$k, weight_penalty=$penalty..."
-            python -m src.run \
+            python3 run.py \
                 --dataset="$Didataset" \
-                --k_plus="$k" \
-                --weight_penalty="$penalty" \
-                --use_best_hyperparams
+                --seed="$k"
+#                --weight_penalty="$penalty"
+#                --use_best_hyperparams
             echo "Finished dataset=$Didataset, k_plus=$k, weight_penalty=$penalty."
         done
     done
