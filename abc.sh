8232e9a7cb33aa755e2fb36ddf7313aff2545025
Qin87 <qinjiang19871018@gmail.com>
2025-08-15 17:25:12 +0100

    Aug15

--- abc.sh
+++ abc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-Direct_dataset=("pokec"  "actor"  "syn-dir")  #  'snap-patents' 'directed-roman-empire'
+#Direct_dataset=("pokec"  "actor"  "syn-dir")  #  'snap-patents' 'directed-roman-empire'
+Direct_dataset=("penn94" "reed98"  "cornell115" "johnshopkins55" "genius")  #  'snap-patents' 'directed-roman-empire'
 Num_layers=(1 2 3 4 5 6 7 8)
 
 alphas=(0 0.5 1 -1)
@@ -17,7 +18,7 @@ for dataset in "${Direct_dataset[@]}"; do
         for beta in "${betas[@]}"; do
             for gamma in "${gammas[@]}"; do
                 echo "Running on dataset=${dataset} with alpha=${alpha}, beta=${beta}, gamma=${gamma}"
-                python3 run.py --dataset "$dataset"    --num_layers "$layers"  --alpha "$alpha" --beta "$beta" --gamma "$gamma"  --weight_penalty="$penalty"
+                python3 run.py --dataset "$dataset" --num_runs 1   --num_layers "$layers"  --alpha "$alpha" --beta "$beta" --gamma "$gamma"  --weight_penalty="$penalty"
             done
             done
             done
